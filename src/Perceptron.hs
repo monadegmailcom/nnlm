c6ef5affc5224afc0d544076a1af4d316b9b1f6a
monade <monade@gmail.com>
2016-04-03 19:13:39 +0200

    implemented batch perceptron

--- src/Perceptron.hs
+++ src/Perceptron.hs
@@ -1,7 +1,6 @@
 module Perceptron
-    ( RecurPerceptron(..),
-      Iteration(..),
-      nextIteration
+    ( runIterations,
+      getHyperplane
     ) where
 
 data RecurPerceptron = RecurPerceptron {
@@ -10,12 +9,64 @@ data RecurPerceptron = RecurPerceptron {
   rightP     :: Maybe RecurPerceptron
 }
 
-data Iteration = Iteration {
-  weights :: [Double],
-  bias :: Double,
-  learningRateF :: Integer -> Double,
-  n :: Integer
-}
+-- product of a scalar with a vector
+smult :: Double -> [Double] -> [Double]
+smult r = map (r *)
 
-nextIteration :: Iteration -> Iteration
-nextIteration it = undefined
+-- euclidian vector norm
+norm :: [Double] -> Double
+norm = sqrt . sum . map (\x -> x * x)
+
+-- sum of two vectors
+vadd :: [Double] -> [Double] -> [Double]
+vadd = zipWith (+)
+
+getHyperplane ::
+  Double ->
+  (Int -> Double) ->            -- learning rate
+  [[Double]] ->
+  [[Double]] ->
+  (Int, [Double], Double)
+getHyperplane eps = (((head . dropWhile p) .) .) . runIterations where
+  p (_, _, e) = e >= eps
+-- (f .) (g x) = (f .) . g $ x
+runIterations ::
+  (Int -> Double) ->            -- learning rate
+  [[Double]] ->
+  [[Double]] ->
+  [(Int, [Double], Double)]
+runIterations etaF ps1 ps2 = iterate f (1, w0, 1.0 / 0) where
+  -- start condition with normalized normal vector
+  w0 = take l $  1.0 : 1.0 : repeat 0.0
+  l =  1 + length (head ps1)
+
+  -- eval all iterations lazily
+  f :: (Int, [Double], Double) -> (Int, [Double], Double)
+  f (n, ws_n, _) = (n + 1, ws_n1, e_n1) where
+    ws_n1 = smult (1.0 / norm (tail ws)) ws
+    ws = vadd ws_n delta
+    e_n1 = max (abs (head delta / head ws_n)) (norm (tail delta))
+    delta = nextDelta ps1 ps2 (etaF n) ws_n
+
+nextDelta ::
+  [[Double]] ->
+  [[Double]] ->
+  Double ->             -- learning rate
+  [Double] ->           -- weights with first element as bias
+  [Double]              -- new weight deltas
+nextDelta ps1 ps2 eta ws_n = smult alpha $ vsub m1 m2 where
+  alpha = eta / fromIntegral (length ps1 + length ps2)
+  m1 = condSum isClass2 ps1 -- sum of missclassified class 1 points
+  m2 = condSum (not . isClass2) ps2 -- sum of missclassified class 2 points
+  condSum p = foldr (f p) nv
+  nv = replicate (1 + (length . head) ps1) 0.0
+
+  f p cs as = if p cs then vadd as (1.0 : cs) else as
+  isClass2 cs = sprod ws_n (1.0 : cs) < 0.0
+
+  -- scalar product of two vectors
+  sprod :: [Double] -> [Double] -> Double
+  sprod as = sum . zipWith (*) as
+
+  -- diff of two vectors
+  vsub = zipWith (-)
--- test/Spec.hs
+++ test/Spec.hs
@@ -41,8 +41,8 @@ pol2Cart ::
   (Double, Double)
 pol2Cart (r, phi) = (r * cos phi, r * sin phi)
 
-prepareTestSample :: IO [(Double, Double)]
-prepareTestSample = do
+sample1 :: IO [(Double, Double)]
+sample1 = do
   let r = 0.5
   let w = 0.1
   let n = 100
@@ -55,12 +55,39 @@ prepareTestSample = do
   let bs = zipWith (curry f) xs ps
   return bs
 
+sample2 :: IO [(Double, Double)]
+sample2 = do
+  let r = 0.5
+  let w = 0.1
+  let n = 100
+  let c = (1.5, -1.0)
+
+  xs <- getUniformRs n (r - w / 2) (r + w / 2)
+  ps <- getUniformRs n pi (2*pi)
+
+  let f = shift c . rotate (0.0,0.0) (pi/4) . pol2Cart
+  let bs = zipWith (curry f) xs ps
+  return bs
+
 main :: IO ()
 main = do
   -- set seed to make sample draw reproducable
   setStdGen $ mkStdGen 42
-  ps <- prepareTestSample
-  plot X11 [ Data2D [Color Red] [] ps]
+  ps1 <- sample1
+  ps2 <- sample2
+  let mkList = map (\(x,y) -> [x,y])
+  let etaF _ = 1.0
+  let ws = take 15 $ runIterations etaF (mkList ps1) (mkList ps2)
+  print "runIterations"
+  print ws
+  print "Hyperplane"
+  print $ getHyperplane 0.1 etaF (mkList ps1) (mkList ps2)
+
+  let hp (_, [w0, w1, w2], _) x = -(w0 + w1 * x) / w2
+  let hs = [Data2D [Title "Class Red",  Color Red]  [] ps1,
+            Data2D [Title "Class Blue", Color Blue] [] ps2]
+  let hps = map (Function2D [Title "", Color Black] [Range 0.6 1.8] . hp) ws
+  --plot X11 $ hs ++ hps
   hspec $
     describe "generate sample data" $
       it "2 d arcs"
